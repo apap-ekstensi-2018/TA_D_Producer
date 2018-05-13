a0cd5256e3bbca3b589ca066329db931b216b1b7
bw <herlen.adita@kokatto.com>
2018-05-13 13:42:30 +0700

    setup resources

---
+++ src/main/java/com/apap/siperpusproducer/SiperpusProducerApplication.java
@@ -0,0 +1,12 @@
+package com.apap.siperpusproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiperpusProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiperpusProducerApplication.class, args);
+	}
+}
